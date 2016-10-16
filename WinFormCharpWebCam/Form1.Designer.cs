aac205b8d24d14c3490ef1f277e1d751ab3d6b53
Tri <Tri@MACs-MacBook-Pro.local>
2016-10-16 10:09:01 +0700

    Completed

---
+++ WinFormCharpWebCam/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WinFormCharpWebCam
+{
+
+    partial class mainWinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgVideo = new System.Windows.Forms.PictureBox();
+            this.bntCapture = new System.Windows.Forms.Button();
+            this.cboDevices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgVideo
+            // 
+            this.imgVideo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgVideo.Location = new System.Drawing.Point(12, 12);
+            this.imgVideo.Name = "imgVideo";
+            this.imgVideo.Size = new System.Drawing.Size(364, 330);
+            this.imgVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgVideo.TabIndex = 0;
+            this.imgVideo.TabStop = false;
+            // 
+            // bntCapture
+            // 
+            this.bntCapture.Location = new System.Drawing.Point(263, 401);
+            this.bntCapture.Name = "bntCapture";
+            this.bntCapture.Size = new System.Drawing.Size(101, 30);
+            this.bntCapture.TabIndex = 5;
+            this.bntCapture.Text = "Capture Image";
+            this.bntCapture.UseVisualStyleBackColor = true;
+            this.bntCapture.Click += new System.EventHandler(this.bntCapture_Click);
+            // 
+            // cboDevices
+            // 
+            this.cboDevices.FormattingEnabled = true;
+            this.cboDevices.Location = new System.Drawing.Point(13, 373);
+            this.cboDevices.Name = "cboDevices";
+            this.cboDevices.Size = new System.Drawing.Size(363, 21);
+            this.cboDevices.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Please select devices:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(145, 401);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 30);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(25, 401);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(101, 30);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // mainWinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboDevices);
+            this.Controls.Add(this.bntCapture);
+            this.Controls.Add(this.imgVideo);
+            this.Name = "mainWinForm";
+            this.Text = "WebCam";
+            this.Load += new System.EventHandler(this.mainWinForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgVideo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgVideo;
+        private System.Windows.Forms.Button bntCapture;
+        private System.Windows.Forms.ComboBox cboDevices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WinFormCharpWebCam/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace WinFormCharpWebCam
+{
+
+    public partial class mainWinForm : Form
+    {
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+
+        public mainWinForm()
+        {
+            InitializeComponent();
+        }
+
+        private void getCamList()
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                cboDevices.Items.Clear();
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in videoDevices)
+                {
+                    cboDevices.Items.Add(device.Name);
+                }
+                cboDevices.SelectedIndex = 0; //make dafault to first cam
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                cboDevices.Items.Add("No capture device on your system");
+            }
+        }
+
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            getCamList();
+        }
+
+        //toggle start and stop button
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text.Equals("Start"))
+            {
+                if (DeviceExist)
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[cboDevices.SelectedIndex].MonikerString);
+                    videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                    CloseVideoSource();
+                    videoSource.DesiredFrameSize = new Size(160, 120);
+                    //videoSource.DesiredFrameRate = 10;
+                    videoSource.Start();
+
+                   // webcam.Start();
+                    btnStart.Text = "Stop";
+                  
+                }
+                else
+                {
+                    label2.Text = "Error: No Device selected.";
+                }
+            }
+            else
+            {
+                if (videoSource.IsRunning)
+                {
+                   //// timer1.Enabled = false;
+                    CloseVideoSource();
+                   // webcam.Stop();
+                    label2.Text = "Device stopped.";
+                    btnStart.Text = "Start";
+                }
+            }
+        }
+
+        //eventhandler if new frame is ready
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            //do processing here
+            imgVideo.Image = img;
+        }
+
+        //close the device safely
+        private void CloseVideoSource()
+        {
+           // webcam.Stop();
+
+            if (!(videoSource == null))
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    videoSource = null;
+                }
+        }
+
+
+        //prevent sudden close while device is running
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseVideoSource();
+        }
+
+        WebCam webcam;
+        private void mainWinForm_Load(object sender, EventArgs e)
+        {
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+            //webcam.Start();
+            getCamList();
+        }
+
+        private void bntCapture_Click(object sender, EventArgs e)
+        {
+            Helper.SaveImageCapture(imgVideo.Image);
+        }
+
+       
+
+
+
+    }
+}
